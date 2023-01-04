29db626209bf224847465e8d8e991dfc717b3514
Alban Charrin <Alban.Charrin@etu.univ-smb.fr>
2023-01-04 08:29:20 +0100

    ouais

--- Trapped in the dark/Game1.cs
+++ Trapped in the dark/Game1.cs
@@ -42,7 +42,7 @@ namespace Trapped_in_the_dark
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.MediumPurple);
 
             // TODO: Add your drawing code here
 
