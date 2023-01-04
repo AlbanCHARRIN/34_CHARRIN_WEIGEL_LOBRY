a74cfcf0a990a650d487e82f0f7350ea9a1819bf
Alban Charrin <Alban.Charrin@etu.univ-smb.fr>
2023-01-04 16:46:03 +0100

    sdhhf

--- Trapped in the dark/Case.cs
+++ Trapped in the dark/Case.cs
@@ -8,6 +8,7 @@ namespace Trapped_in_the_dark
 {
     internal class Case
     {
+        private const int dimension = 6;
         private bool nord, est, ouest, sud;
         private int valeurcase;
 
@@ -83,6 +84,110 @@ namespace Trapped_in_the_dark
         {
             return obj is Case @case &&
                    Valeurcase == @case.Valeurcase;
-        }  
+        }
+        public int[,] GenerateurDuTileset()
+        {
+            int[,] tilesets = new int[4 * dimension + 2, 4 * dimension + 2];
+            Case[,] tileset = new Case[dimension, dimension];
+            int compteur = 0;
+            int compteurvaleur = 0;
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    tileset[i, j] = new Case(false, false, false, false, compteurvaleur);
+                    compteurvaleur++;
+                }
+            }
+            while (compteur < dimension * dimension - 1)
+            {
+                Random rand = new Random();
+                Random rand2 = new Random();
+                Random direction = new Random();
+                int randnext = rand.Next(0, dimension - 1);
+                int rand2next = rand2.Next(0, dimension - 1);
+                int directionnext = direction.Next(0, 4);
+                if (directionnext == 1)
+                {
+                    if (tileset[randnext, rand2next] != tileset[randnext - 1, rand2next])
+                    {
+                        tileset[randnext - 1, rand2next].Valeurcase = tileset[randnext, rand2next].Valeurcase;
+                        tileset[randnext, rand2next].Nord = true;
+                        tileset[randnext - 1, rand2next].Sud = true;
+                        compteur++;
+                    }
+                }
+                else if (directionnext == 2)
+                {
+                    if (tileset[randnext, rand2next] != tileset[randnext, rand2next + 1])
+                    {
+                        tileset[randnext - 1, rand2next + 1].Valeurcase = tileset[randnext, rand2next].Valeurcase;
+                        tileset[randnext - 1, rand2next].Est = true;
+                        tileset[randnext - 1, rand2next + 1].Ouest = true;
+                        compteur++;
+                    }
+                }
+                else if (directionnext == 3)
+                {
+                    if (tileset[randnext, rand2next] != tileset[randnext + 1, rand2next])
+                    {
+                        tileset[randnext + 1, rand2next].Valeurcase = tileset[randnext, rand2next].Valeurcase;
+                        tileset[randnext, rand2next].Sud = true;
+                        tileset[randnext + 1, rand2next].Nord = true;
+                        compteur++;
+                    }
+                }
+                else if (directionnext == 4)
+                {
+                    if (tileset[randnext, rand2next] != tileset[randnext, rand2next - 1])
+                    {
+                        tileset[randnext - 1, rand2next - 1].Valeurcase = tileset[randnext, rand2next].Valeurcase;
+                        tileset[randnext - 1, rand2next].Ouest = true;
+                        tileset[randnext - 1, rand2next - 1].Est = true;
+                        compteur++;
+                    }
+                }
+            }
+            for (int i = 0; i < tilesets.GetLength(0); i++)
+            {
+                for (int j = 0; j < tilesets.GetLength(1); j++)
+                    tilesets[i, j] = 0;
+            }
+            for (int i = 1; i < tileset.GetLength(0); i++)
+            {
+                for (int j = 1; j < tileset.GetLength(1); j++)
+                {
+                    if (tileset[i - 1, j - 1].Est == true)
+                    {
+                        tilesets[i * 3 + 1, j * 3 + 1] = 1;
+                        tilesets[i * 3 + 1, j * 3 + 2] = 1;
+                        tilesets[i * 3 + 1, j * 3 + 3] = 1;
+                        tilesets[i * 3 + 1, j * 3 + 4] = 1;
+                    }
+                    if (tileset[i - 1, j - 1].Sud == true)
+                    {
+                        tilesets[i * 3 + 1, j * 3 + 1] = 1;
+                        tilesets[i * 3 + 2, j * 3 + 1] = 1;
+                        tilesets[i * 3 + 3, j * 3 + 1] = 1;
+                        tilesets[i * 3 + 4, j * 3 + 1] = 1;
+                    }
+                    if (tileset[i - 1, j - 1].Nord == true)
+                    {
+                        tilesets[i * 3 + 1, j * 3 - 1] = 1;
+                        tilesets[i * 3 + 2, j * 3 - 1] = 1;
+                        tilesets[i * 3 + 3, j * 3 - 1] = 1;
+                        tilesets[i * 3 + 4, j * 3 - 1] = 1;
+                    }
+                    if (tileset[i - 1, j - 1].Ouest == true)
+                    {
+                        tilesets[i * 3 - 1, j * 3 + 1] = 1;
+                        tilesets[i * 3 - 1, j * 3 + 2] = 1;
+                        tilesets[i * 3 - 1, j * 3 + 3] = 1;
+                        tilesets[i * 3 - 1, j * 3 + 4] = 1;
+                    }
+                }
+            }
+            return tilesets;
+        }
     }
 }
