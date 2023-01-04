eb263e6d565aeedc14b35d9fa6d1281403f0b714
Esteban Lobry <esteban.lobry@etu.univ-smb.fr>
2023-01-04 10:21:20 +0100

    push

--- Trapped in the dark/Menu.cs
+++ Trapped in the dark/Menu.cs
@@ -7,21 +7,36 @@ using MonoGame.Extended.Screens;
 public class Menu : GameScreen
 {
     private Game _myGame;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _background;
+
     // pour récupérer une référence à l’objet game pour avoir accès à tout ce qui est
     // défini dans Game1
     public Menu(Game game) : base(game)
     {
         _myGame = game;
+
     }
     public override void LoadContent()
     {
+        _background = Content.Load<Texture2D>("background.jpg");
         base.LoadContent();
     }
     public override void Update(GameTime gameTime)
-    { }
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
     public override void Draw(GameTime gameTime)
     {
-        _myGame.GraphicsDevice.Clear(Color.SaddleBrown); // on utilise la reference vers
-                                                 // Game1 pour chnager le graphisme
+
+        _myGame.GraphicsDevice.Clear(Color.SaddleBrown);
+        _spriteBatch.Begin();
+
+
+        _spriteBatch.Draw(_background, new Vector2(0, 0), Microsoft.Xna.Framework.Color.White);
+
+        _spriteBatch.End();
+
     }
+
 }
