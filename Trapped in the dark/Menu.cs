8a021d836a54d364c2a3d16272ee9ed9d1bcc30d
Romain Weigel <Romain.Weigel@etu.univ-savoie.fr>
2023-01-04 16:33:54 +0100

    n

--- Trapped in the dark/Menu.cs
+++ Trapped in the dark/Menu.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Screens;
+
+
+public class Menu : GameScreen
+{
+    private Game _myGame;
+    private SpriteBatch _spriteBatch { get; set; }
+    private Texture2D _background;
+
+
+
+
+    // pour récupérer une référence à l’objet game pour avoir accès à tout ce qui est
+    // défini dans Game1
+    public Menu(Game game) : base(game)
+    {
+        _myGame = game;
+    
+
+    }
+   
+    public override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _background = Content.Load<Texture2D>("background");
+        base.LoadContent();
+    }
+   
+    public override void Update(GameTime gameTime)
+    {
+    }
+    public override void Draw(GameTime gameTime)
+    {
+
+        _myGame.GraphicsDevice.Clear(Color.SaddleBrown);
+        _spriteBatch.Begin();
+
+
+        _spriteBatch.Draw(_background, new Vector2(0, 0), Microsoft.Xna.Framework.Color.White);
+
+        _spriteBatch.End();
+
+    }
+
+}
