d8b4b8dc12719d93ad8822a0a5e60deed146535f
Esteban Lobry <esteban.lobry@etu.univ-smb.fr>
2023-01-04 08:33:10 +0100

    daz

--- Trapped in the dark/Game1.cs
+++ Trapped in the dark/Game1.cs
@@ -42,7 +42,7 @@ namespace Trapped_in_the_dark
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.MediumPurple);
+            GraphicsDevice.Clear(Color.Teal);
 
             // TODO: Add your drawing code here
 
