d49e1fa1705945d610fe8e7a629acfc04a4c7b10
Alban Charrin <Alban.Charrin@etu.univ-smb.fr>
2023-01-04 10:23:07 +0100

    gkgk

---
+++ Trapped in the dark/Case.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapped_in_the_dark
+{
+    internal class Case
+    {
+        private bool nord, est, ouest, sud;
+        private int valeurcase;
+    }
+}
