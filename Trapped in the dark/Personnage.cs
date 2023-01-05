2f369fc5fa78cf287670ed020474ada48e96ca1d
Romain Weigel <Romain.Weigel@etu.univ-savoie.fr>
2023-01-05 08:26:09 +0100

    jj

--- Trapped in the dark/Personnage.cs
+++ Trapped in the dark/Personnage.cs
@@ -14,6 +14,10 @@ public class Personnage : GameScreen
     private AnimatedSprite _perso;
     // pour récupérer une référence à l’objet game pour avoir accès à tout ce qui est
     // défini dans Game1
+    private double acceleration = 1;
+    private double positionPersoX = 0;
+    private double positionPersoY = 0;
+
     public Personnage(Game game) : base(game)
     {
         _myGame = game;
@@ -31,8 +35,56 @@ public class Personnage : GameScreen
     }
     public override void Update(GameTime gameTime)
     {
-        _perso.Play("left"); // une des animations définies dans « animation.sf »
+        KeyboardState keyboardState = Keyboard.GetState();
+
+        positionPersoX = 0;
+        positionPersoY = 0;
+
+        if (keyboardState.IsKeyDown(Keys.Left))
+        {
+            _perso.Play("left");
+            positionPersoX = 0;
+            positionPersoY = -10;
+
+        }
+
+        else if (keyboardState.IsKeyDown(Keys.Right))
+        {
+            _perso.Play("right");
+            positionPersoX = 0;
+            positionPersoY = 10;
+        }
+
+        else if (keyboardState.IsKeyDown(Keys.Up))
+        {
+            _perso.Play("up");
+            positionPersoX = -10;
+            positionPersoY = 0;
+        }
+        else if (keyboardState.IsKeyDown(Keys.Down))
+        {
+            _perso.Play("down");
+            positionPersoX = 10;
+            positionPersoY = 0;
+        }
+        else
+        {
+            _perso.Play("idle");
+        }
+
+        if (keyboardState.IsKeyDown(Keys.LeftShift))
+        {
+            acceleration = 1.5;
+        }
+        else if (keyboardState.IsKeyUp(Keys.LeftShift))
+        {
+            acceleration = 1;
+        }
+
+
         _perso.Update(gameTime);
+
+        _positionPerso += new Vector2((float)acceleration * (float)positionPersoY, (float)acceleration * (float)positionPersoX);
     }
     public override void Draw(GameTime gameTime)
     {
