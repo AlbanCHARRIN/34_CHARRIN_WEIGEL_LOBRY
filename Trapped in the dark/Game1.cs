14b884bc0d755a98988c565e88c7e6580dddf254
Esteban Lobry <esteban.lobry@etu.univ-smb.fr>
2023-01-11 09:07:51 +0100

    Random

--- Trapped in the dark/Game1.cs
+++ Trapped in the dark/Game1.cs
@@ -24,6 +24,10 @@ namespace Trapped_in_the_dark
         private Personnage _personnage;
         private Map1 _map1;
         private Map2 _map2;
+        private Map3 _map3;
+        private Map4 _map4;
+        private Map5 _map5;
+        private Map6 _map6;
 
         public GraphicsDeviceManager _graphics;
         public int[,] _tableauMur;
@@ -70,6 +74,12 @@ namespace Trapped_in_the_dark
             _menu = new Menu(this);
             _pause = new Pause(this);
             _personnage = new Personnage(this);
+            _map1 = new Map1(this);
+            _map2 = new Map2(this);
+            _map3 = new Map3(this);
+            _map4 = new Map4(this);
+            _map5 = new Map5(this);
+            _map6 = new Map6(this);
         }
 
         protected override void Initialize()
@@ -112,11 +122,52 @@ namespace Trapped_in_the_dark
             KeyboardState keyboardState = Keyboard.GetState();
 
             MouseState _mouseState = Mouse.GetState();
-            if (_mouseState.LeftButton == ButtonState.Pressed)
+
+            Random random = new Random();
+            int randomMap = random.Next(1, 7);
+            int[] tabMap = new int[6] {0,0,0,0,0,0};
+
+            for ( int i = 0; i < tabMap.Length; i++)
+            {
+                if (tabMap[i] == i+1 )
+                    random.Next(1, 7);
+            }
+            //mouseState.LeftButton == ButtonState.Pressed
+            if (keyboardState.IsKeyDown(Keys.F6))
             {
                 if (this.Etat == Etats.Play)
-                    _screenManager.LoadScreen(_personnage, new FadeTransition(GraphicsDevice, Color.Black));
-
+                    //_screenManager.LoadScreen(_personnage, new FadeTransition(GraphicsDevice, Color.Black));
+                    if(randomMap == 1)
+                    {
+                        tabMap[0] = 1;
+                        _screenManager.LoadScreen(_map1, new FadeTransition(GraphicsDevice, Color.Black));
+                        
+                    }
+                    if (randomMap == 2)
+                    {
+                    tabMap[1] = 2;
+                    _screenManager.LoadScreen(_map2, new FadeTransition(GraphicsDevice, Color.Black));
+                    }
+                if (randomMap == 3)
+                {
+                    tabMap[2] = 3;
+                    _screenManager.LoadScreen(_map3, new FadeTransition(GraphicsDevice, Color.Black));
+                }
+                if (randomMap == 4)
+                {
+                    tabMap[3] = 4;
+                    _screenManager.LoadScreen(_map4, new FadeTransition(GraphicsDevice, Color.Black));
+                }
+                if (randomMap == 5)
+                {
+                    tabMap[4] = 5;
+                    _screenManager.LoadScreen(_map5, new FadeTransition(GraphicsDevice, Color.Black));
+                }
+                if (randomMap == 6)
+                {
+                    tabMap[5] = 6;
+                    _screenManager.LoadScreen(_map6, new FadeTransition(GraphicsDevice, Color.Black));
+                }
 
 
                 else if (this.Etat == Etats.Quit)
@@ -140,9 +191,9 @@ namespace Trapped_in_the_dark
                 Color.Black));
             }
 
-            else if (keyboardState.IsKeyDown(Keys.F2))
+            else if (keyboardState.IsKeyDown(Keys.F3))
             {
-                _screenManager.LoadScreen(_pause, new FadeTransition(GraphicsDevice,
+                _screenManager.LoadScreen(_map1, new FadeTransition(GraphicsDevice,
                 Color.Black));
             }
 
